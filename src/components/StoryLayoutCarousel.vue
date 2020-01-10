4f5bd0cddf15b5961c47991d9bd13f83c783fbfa
twelvepics <net.sboobi@gmail.com>
2020-01-11 01:33:48 +0800

    pics size

--- src/components/StoryLayoutCarousel.vue
+++ src/components/StoryLayoutCarousel.vue
@@ -1,158 +1,161 @@
 <template>
-    <div class="content" style="margin:30px 0 0 0;padding:0 25px 25px 25px">
-        <!-- Slider main container -->
+  <div class="content" style="margin:30px 0 0 0;padding:0 25px 25px 25px">
+    <!-- Slider main container -->
+    <div
+      class="swiper-container"
+      ref="swiper-container"
+      style="padding-bottom:0;margin-bottom:25px;border:1px solid #ccc;"
+    >
+      <!-- Additional required wrapper -->
+      <div class="swiper-wrapper">
         <div
-            class="swiper-container"
-            ref="swiper-container"
-            style="padding-bottom:0;margin-bottom:25px;border:1px solid #ccc;"
-        >
-            <!-- Additional required wrapper -->
-            <div class="swiper-wrapper">
-                <div
-                    v-for="(pic, index) in pics"
-                    :key="index"
-                    class="swiper-slide"
-                    :style="{
+          v-for="(pic, index) in pics"
+          :key="index"
+          class="swiper-slide"
+          :style="{
                         width: `${getThumbSize(pic.small).width}px`,
                         height: `${getThumbSize(pic.small).height}px`
                     }"
-                >
-                    <!-- Slide Content -->
-                    <img
-                        :src="pic.small.web_path"
-                        style="cursor:pointer;"
-                        :style="{
+        >
+          <!-- Slide Content -->
+          <img
+            :src="pic.small.web_path"
+            style="cursor:pointer;"
+            :style="{
                             'max-width': `${getThumbSize(pic.small).width}px`,
                             'max-height': `${getThumbSize(pic.small).height}px`,
                             border: '1px solid #777'
                         }"
-                        @click.self="imageClicked(index)"
-                    />
-                    <!-- Slide {{ index }} -->
-                </div>
-            </div>
-            <!-- END Additional required wrapper -->
-            <!-- If we need navigation buttons -->
-            <div class="swiper-button-prev" style="color:red;top:40%;left:.3rem;"></div>
-            <div class="swiper-button-next" style="color:red;top:40%;right:.3rem;"></div>
+            @click.self="imageClicked(index)"
+          />
+          <!-- Slide {{ index }} -->
         </div>
-
-        <!-- End Slider main container -->
-
-        <!-- bottom selected image -->
-        <div class="slideshow-pic-container">
-            <div class="slideshow-pic">
-                <img :src="pics[0].large.web_path" style="max-height:650px;border:1px solid #aaa;" ref="top_pic" alt />
-                <p
-                    class="caption"
-                    :style="{ 'max-width': `${getCaptionWidth()}px` }"
-                    style="text-align:left;"
-                    ref="top_caption"
-                >
-                    {{ pics[0].caption }}
-                </p>
-            </div>
-        </div>
-        <!-- ends bottom selected image -->
+      </div>
+      <!-- END Additional required wrapper -->
+      <!-- If we need navigation buttons -->
+      <div class="swiper-button-prev" style="color:red;top:40%;left:.3rem;"></div>
+      <div class="swiper-button-next" style="color:red;top:40%;right:.3rem;"></div>
     </div>
+
+    <!-- End Slider main container -->
+
+    <!-- bottom selected image -->
+    <div class="slideshow-pic-container">
+      <div class="slideshow-pic">
+        <img
+          :src="pics[0].large.web_path"
+          style="max-height:650px;border:1px solid #aaa;"
+          ref="top_pic"
+          alt
+        />
+        <p
+          class="caption"
+          :style="{ 'max-width': `${getCaptionWidth()}px` }"
+          style="text-align:left;"
+          ref="top_caption"
+        >{{ pics[0].caption }}</p>
+      </div>
+    </div>
+    <!-- ends bottom selected image -->
+  </div>
 </template>
 
 <script>
 import Swiper from "swiper";
 
 export default {
-    props: ["pics"],
-    data() {
-        return {
-            carousel: null,
-            selectedPicId: 0
-        };
-    },
+  props: ["pics"],
+  data() {
+    return {
+      carousel: null,
+      selectedPicId: 0
+    };
+  },
 
-    methods: {
-        picOrientation(pic) {
-            return pic.width >= pic.height ? "horizontal" : "vertical";
-        },
-        getThumbSize(pic) {
-            const _h = pic.height;
-            const _w = pic.width;
-            let width, height, divider;
-            if (_w >= _h) {
-                // horizontal, make it 126 high
-                divider = _h / 126;
-            } else {
-                // vertical, make it 150 high
-                divider = _h / 160;
-            }
-            width = Math.floor(_w / divider);
-            height = Math.floor(_h / divider);
-            return { width, height };
-        },
-        thumbClicked(idx) {
-            const top_pic = this.$refs.top_pic;
-            // console.log(top_pic);
-            // console.log(top_pic.src);
-            this.selectedPicId = idx;
-            top_pic.src = this.pics[idx].large.web_path;
-            this.$refs.top_caption.textContent = this.pics[idx].caption;
-        },
-        getCaptionWidth(maxHeight = 650) {
-            const pic = this.pics[this.selectedPicId].large;
-            const _h = pic.height;
-            const _w = pic.width;
-            let width, divider;
-            divider = _h / maxHeight;
-            width = Math.floor(_w / divider);
-            return width;
-        },
-        imageClicked(idx) {
-            // console.log("PIC CLICKED => " + idx);
-            this.thumbClicked(idx);
-        }
+  methods: {
+    picOrientation(pic) {
+      return pic.width >= pic.height ? "horizontal" : "vertical";
     },
-    mounted() {
-        // console.log("GO");
-        // console.log(this.$refs["swiper-container"]);
-        // const vm = this;
-        this.carousel = new Swiper(this.$refs["swiper-container"], {
-            slidesPerView: "auto",
-            spaceBetween: 15,
-            grabCursor: true,
-            navigation: {
-                nextEl: ".swiper-button-next",
-                prevEl: ".swiper-button-prev"
-            }
-        });
-        // console.log(this.carousel);
+    getThumbSize(pic) {
+      const _h = pic.height;
+      const _w = pic.width;
+      let width, height, divider;
+      if (_w >= _h) {
+        // horizontal, make it 126 high
+        divider = _h / 126;
+      } else {
+        // vertical, make it 150 high
+        divider = _h / 160;
+      }
+      width = Math.floor(_w / divider);
+      height = Math.floor(_h / divider);
+      return { width, height };
+    },
+    thumbClicked(idx) {
+      const top_pic = this.$refs.top_pic;
+      // console.log(top_pic);
+      // console.log(top_pic.src);
+      this.selectedPicId = idx;
+      top_pic.src = this.pics[idx].large.web_path;
+      this.$refs.top_caption.textContent = this.pics[idx].caption;
+    },
+    getCaptionWidth(maxHeight = 650) {
+      const pic = this.pics[this.selectedPicId].large;
+      const _h = pic.height;
+      const _w = pic.width;
+      let width, divider;
+      divider = _h / maxHeight;
+      width = Math.floor(_w / divider);
+      return width;
+    },
+    imageClicked(idx) {
+      // console.log("PIC CLICKED => " + idx);
+      this.thumbClicked(idx);
     }
+  },
+  mounted() {
+    // console.log("GO");
+    // console.log(this.$refs["swiper-container"]);
+    // const vm = this;
+    this.carousel = new Swiper(this.$refs["swiper-container"], {
+      slidesPerView: "auto",
+      spaceBetween: 15,
+      grabCursor: true,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+      }
+    });
+    // console.log(this.carousel);
+  }
 };
 </script>
 
 <style scoped>
 .caption {
-    font-size: 0.85rem;
+  font-size: 0.85rem;
 }
 .swiper-slide {
-    text-align: center;
-    font-size: 18px;
-    background: #fff;
+  text-align: center;
+  font-size: 18px;
+  background: #fff;
 }
 .swiper-container {
-    width: 100%;
-    height: 100%;
+  width: 100%;
+  height: 100%;
 }
 
 .slideshow-pic-container {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
 }
 
 /* media queries */
 @media only screen and (min-width: 1024px) {
-    .story-padding {
-        padding: 0 5rem 0 5rem;
-    }
+  .story-padding {
+    padding: 0 5rem 0 5rem;
+  }
 }
 </style>
--- src/components/StoryLayoutTiles.vue
+++ src/components/StoryLayoutTiles.vue
@@ -1,35 +1,39 @@
 <template>
-    <div class="content" style="margin:30px 0 30px 0;line-height:1.3rem;">
-        <div class="tiles-layout" v-show="!isImageSelected">
-            <div v-for="(row, rowIdx) in tiles" :key="rowIdx" :style="getRowStyle(rowIdx)">
-                <div v-for="(pic, picIdx) in row" :key="picIdx">
-                    <!-- 
+  <div class="content" style="margin:30px 0 30px 0;line-height:1.3rem;">
+    <div class="tiles-layout" v-show="!isImageSelected">
+      <div v-for="(row, rowIdx) in tiles" :key="rowIdx" :style="getRowStyle(rowIdx)">
+        <div v-for="(pic, picIdx) in row" :key="picIdx">
+          <!-- 
           --- {{rowIdx}}/{{picIdx}} ---
           -->
-                    <div class="pic">
-                        <img :src="tiles[rowIdx][picIdx].web_path" @click="imageClicked(rowIdx, picIdx)" />
-                    </div>
-                </div>
-            </div>
+          <div class="pic">
+            <img :src="tiles[rowIdx][picIdx].web_path" @click="imageClicked(rowIdx, picIdx)" />
+          </div>
         </div>
-        <div class="content" v-show="isImageSelected" style="width:100%;text-align: center;">
-            <div
-                :style="{
-                    'max-width': picOrientation(pics[imageSelected].large) === 'horizontal' ? '900px' : '450px',
+      </div>
+    </div>
+    <div class="content" v-show="isImageSelected" style="width:100%;text-align: center;">
+      <div
+        :style="{
+                    'max-width': picOrientation(pics[imageSelected].large) === 'horizontal' ? '850px' : '450px',
                     display: 'inline-block',
                     position: 'relative'
                 }"
-            >
-                <a class="delete is-medium" style="position:absolute;right:5px;top:5px;" @click="closeImage"></a>
-                <img
-                    :src="pics[imageSelected].large.web_path"
-                    ref="imageSelected"
-                    :width="picOrientation(pics[imageSelected].large) === 'horizontal' ? '900px' : '450px'"
-                />
-                <div class="caption" style="text-align:left;">{{ pics[imageSelected].caption }}</div>
-            </div>
-        </div>
+      >
+        <a
+          class="delete is-medium"
+          style="position:absolute;right:5px;top:5px;"
+          @click="closeImage"
+        ></a>
+        <img
+          :src="pics[imageSelected].large.web_path"
+          ref="imageSelected"
+          :width="picOrientation(pics[imageSelected].large) === 'horizontal' ? '850px' : '450px'"
+        />
+        <div class="caption" style="text-align:left;">{{ pics[imageSelected].caption }}</div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
@@ -41,84 +45,84 @@
 //     fullhd: Infinity,
 //   }
 export default {
-    props: ["pics", "tiles"],
-    data() {
-        return {
-            debug: false,
-            isImageSelected: false,
-            imageSelected: 0
-        };
+  props: ["pics", "tiles"],
+  data() {
+    return {
+      debug: false,
+      isImageSelected: false,
+      imageSelected: 0
+    };
+  },
+  methods: {
+    getRowStyle(idx) {
+      const colStr = this.tiles[idx]
+        .map(p => {
+          // resize depending on screen size
+          console.log(`Screen" ${this.$mq}`);
+          if (this.$mq === "widescreen") {
+            return `${Math.round((p.tile_width / 5) * 4)}px`;
+          } else if (this.$mq === "desktop") {
+            return `${Math.round((p.tile_width / 16) * 11)}px`;
+          } else if (this.$mq === "tablet") {
+            return `${Math.round(p.tile_width / 3) * 2}px`;
+          } else if (this.$mq === "mobile") {
+            return `${Math.round((p.tile_width / 12) * 5)}px`;
+          }
+          return `${p.tile_width}px`; // fullhd
+        })
+        .join(" ");
+      // console.log(colStr);
+      return {
+        display: "grid",
+        "grid-template-columns": colStr,
+        "grid-column-gap": "5px"
+      };
     },
-    methods: {
-        getRowStyle(idx) {
-            const colStr = this.tiles[idx]
-                .map(p => {
-                    // resize depending on screen size
-                    console.log(`Screen" ${this.$mq}`);
-                    if (this.$mq === "widescreen") {
-                        return `${Math.round((p.tile_width / 5) * 4)}px`;
-                    } else if (this.$mq === "desktop") {
-                        return `${Math.round((p.tile_width / 16) * 11)}px`;
-                    } else if (this.$mq === "tablet") {
-                        return `${Math.round(p.tile_width / 3) * 2}px`;
-                    } else if (this.$mq === "mobile") {
-                        return `${Math.round((p.tile_width / 12) * 5)}px`;
-                    }
-                    return `${p.tile_width}px`; // fullhd
-                })
-                .join(" ");
-            // console.log(colStr);
-            return {
-                display: "grid",
-                "grid-template-columns": colStr,
-                "grid-column-gap": "5px"
-            };
-        },
-        imageClicked(rowIdx, picIdx) {
-            // console.log(`Image clicked ${rowIdx}/${picIdx}`);
-            this.isImageSelected = true;
-            // get id of image
-            let idx = 0;
-            for (let i = 0; i < rowIdx; i++) {
-                idx += this.tiles[i].length;
-            }
-            idx += picIdx;
-            // console.log(this.pics[idx].large.web_path);
-            // console.log(this.$refs);
-            this.imageSelected = idx;
-            this.$refs.imageSelected.src = this.pics[idx].large.web_path;
-        },
-        closeImage() {
-            this.isImageSelected = false;
-            this.imageSelected = 0;
-        },
-        picOrientation(pic) {
-            return pic.width >= pic.height ? "horizontal" : "vertical";
-        }
+    imageClicked(rowIdx, picIdx) {
+      // console.log(`Image clicked ${rowIdx}/${picIdx}`);
+      this.isImageSelected = true;
+      // get id of image
+      let idx = 0;
+      for (let i = 0; i < rowIdx; i++) {
+        idx += this.tiles[i].length;
+      }
+      idx += picIdx;
+      // console.log(this.pics[idx].large.web_path);
+      // console.log(this.$refs);
+      this.imageSelected = idx;
+      this.$refs.imageSelected.src = this.pics[idx].large.web_path;
     },
-    mounted() {
-        console.log("#--- pics tiles mounted ---#");
-        // console.log(this.pics);
-        // console.log(this.tiles);
+    closeImage() {
+      this.isImageSelected = false;
+      this.imageSelected = 0;
+    },
+    picOrientation(pic) {
+      return pic.width >= pic.height ? "horizontal" : "vertical";
     }
+  },
+  mounted() {
+    console.log("#--- pics tiles mounted ---#");
+    // console.log(this.pics);
+    // console.log(this.tiles);
+  }
 };
 </script>
 <style scoped>
 .tiles-layout {
-    display: grid;
-    grid-template-columns: 1fr;
-    justify-items: center;
+  display: grid;
+  grid-template-columns: 1fr;
+  justify-items: center;
 }
 .pic img {
-    cursor: pointer;
+  cursor: pointer;
 }
 .caption {
-    font-size: 0.85rem;
+  font-size: 0.85rem;
 }
 /* media queries */
 @media only screen and (min-width: 1024px) {
-    .story-padding {
-        padding: 0 5rem 0 5rem;
-    }
+  .story-padding {
+    padding: 0 5rem 0 5rem;
+  }
 }
 </style>
