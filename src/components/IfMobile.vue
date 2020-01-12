a6a29fd3d0e147cdb4fb8c39fa71517f1a63f1d1
twelvepics <net.sboobi@gmail.com>
2020-01-12 11:10:55 +0800

    mobile message

---
+++ src/components/IfMobile.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <article class="message is-danger" style="margin:2rem 1rem 0 1rem">
+            <div class="message-header">
+                <p>Mobile version is under works</p>
+            </div>
+            <div class="message-body" style="color:black">
+                Twelvepics beta is only functional on desktop for now. We work hard to provide mobile versions for
+                Android and IOS. Until then please use a desktop environment to browse the site and post stories.
+                <b>Stay tuned.</b>
+            </div>
+        </article>
+    </div>
+</template>
