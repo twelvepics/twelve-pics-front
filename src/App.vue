a2f92d20167f40a6283d0a06ac5d2d57c0306cf3
alain@imac <alain@imac.com>
2020-04-20 16:45:07 +0800

    indent / upgraded packages

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
-    <div id="app" v-if="!is_mobile">
-        <app-header></app-header>
-        <router-view />
-        <router-view name="footer"></router-view>
-        <router-view name="storyModal"></router-view>
-    </div>
-    <div id="app" v-else><if-mobile /></div>
+  <div id="app" v-if="!is_mobile">
+    <app-header></app-header>
+    <router-view />
+    <router-view name="footer"></router-view>
+    <router-view name="storyModal"></router-view>
+  </div>
+  <div id="app" v-else>
+    <if-mobile />
+  </div>
 </template>
 
 <script>
@@ -13,86 +15,91 @@ import UAParser from "ua-parser-js";
 import Header from "./components/Header.vue";
 import IfMobile from "./components/IfMobile.vue";
 export default {
-    components: {
-        appHeader: Header,
-        IfMobile: IfMobile
-    },
-    computed: {
-        // a computed getter
-        is_mobile: function() {
-            const result = UAParser(window.navigator.userAgent);
-            // console.log(`device type -> ${result.device.type}`);
-            const is_mobile = ["console", "mobile", "tablet", "smarttv", "wearable", "embedded"].includes(
-                result.device.type
-            );
-            // console.log(`is mobile -> ${is_mobile}`);
-            return is_mobile;
-        }
+  components: {
+    appHeader: Header,
+    IfMobile: IfMobile
+  },
+  computed: {
+    // a computed getter
+    is_mobile: function() {
+      const result = UAParser(window.navigator.userAgent);
+      // console.log(`device type -> ${result.device.type}`);
+      const is_mobile = [
+        "console",
+        "mobile",
+        "tablet",
+        "smarttv",
+        "wearable",
+        "embedded"
+      ].includes(result.device.type);
+      // console.log(`is mobile -> ${is_mobile}`);
+      return is_mobile;
     }
+  }
 };
 </script>
 
 <style lang="scss">
 /**** layout ****/
 main {
-    margin-top: 100px;
+  margin-top: 100px;
 }
 /**** spacing  ****/
 .mt20 {
-    margin-top: 20px;
+  margin-top: 20px;
 }
 .mt10p20 {
-    margin-top: 10px;
-    padding-bottom: 20px;
+  margin-top: 10px;
+  padding-bottom: 20px;
 }
 .fa-icon-pr4 {
-    padding-right: 4px;
+  padding-right: 4px;
 }
 .fa-icon-pr7 {
-    padding-right: 7px;
+  padding-right: 7px;
 }
 .mr15 {
-    margin-right: 15px;
+  margin-right: 15px;
 }
 .ml10 {
-    margin-left: 10px;
+  margin-left: 10px;
 }
 .pb5 {
-    padding-bottom: 5px;
+  padding-bottom: 5px;
 }
 .m-30-0-15-0 {
-    margin: 30px 0 15px 0;
+  margin: 30px 0 15px 0;
 }
 .pb-05 {
-    padding-bottom: 0.5rem;
+  padding-bottom: 0.5rem;
 }
 .pb-03 {
-    padding-bottom: 0.3rem;
+  padding-bottom: 0.3rem;
 }
 .pt-0 {
-    padding-top: 0;
+  padding-top: 0;
 }
 .message-header {
-    font-weight: 400 !important;
+  font-weight: 400 !important;
 }
 /*********** Toast  *********/
 .fade-enter-active,
 .fade-leave-active {
-    transition-property: opacity;
-    transition-duration: 0.3s;
+  transition-property: opacity;
+  transition-duration: 0.3s;
 }
 .fade-leave-active {
-    transition-duration: 0s;
+  transition-duration: 0s;
 }
 /* .fade-enter-active {
   transition-delay: 0.25s;
 } */
 .fade-enter,
 .fade-leave-active {
-    opacity: 0;
+  opacity: 0;
 }
 /*************** errors  *************/
 .isError {
-    color: red;
+  color: red;
 }
 </style>
--- src/router.js
+++ src/router.js
@@ -7,196 +7,198 @@ import StoryModal from './components/StoryModal.vue'
 
 Vue.use(Router)
 
-export default new Router(
-  {
-    mode: 'history',
-    base: process.env.BASE_URL,
-    routes: [
-      // home page
-      {
-        path: '/',
-        name: 'home',
-        component: Home,
-        meta: {
-          isModalView: true
-        }
-      },
-      {
-        path: '/search',
-        name: 'search',
-        components: {
-          default: () => import(/* webpackChunkName: "search-stories" */ './views/Search.vue'),
-        },
-        meta: {
-          isModalView: true
-        }
-      },
-      // list my stories
-      {
-        path: '/user/:username/stories',
-        name: 'user-stories',
-        components: {
-          default: () => import(/* webpackChunkName: "user-stories" */ './views/UserStoriesList.vue'),
-          footer: () => import(/* webpackChunkName: "footer" */ './views/Footer.vue'),
-        },
-        meta: {
-          isModalView: true
-        }
-      },
-      {
-        path: '/user/:username/starred',
-        name: 'starred',
-        components: {
-          default: () => import(/* webpackChunkName: "starred-stories" */ './views/Starred.vue'),
-        },
-        meta: {
-          isModalView: true
-        }
-      },
-      // create a story
-      {
-        path: '/story/create',
-        name: 'create-story',
-        components: {
-          default: () => import(/* webpackChunkName: "create-story" */ './views/CreateUpdateStory.vue'),
-          footer: () => import(/* webpackChunkName: "footer" */ './views/Footer.vue'),
-        },
-      },
-      // view a story
-      {
-        path: '/story/:slug',
-        name: 'view-story',
-        components: {
-          // default: () => import(/* webpackChunkName: "story" */ './views/StoryStandalone.vue'),
-          footer: () => import(/* webpackChunkName: "footer" */ './views/Footer.vue'),
-        },
-        beforeEnter: (to, from, next) => {
-          const isModalView = from.matched.some(view => view.meta && view.meta.isModalView);
-          // console.log(from.matched);
-          if (!isModalView) {
-            // For direct access
-            to.matched[0].components = {
-              default: StoryStandalone,
-              modal: false
-            }
-          }
-          if (isModalView) {
-            // For isModalView access
-            if (to.matched[0].components) {
-              // Rewrite components for `default`
-              to.matched[0].components.default = from.matched[0].components.default
-              // Rewrite components for `modal`
-              to.matched[0].components.storyModal = StoryModal;
-            }
-          }
-          next();
-        },
-      },
-      {
-        path: '/story/:key/edit',
-        name: 'edit-story',
-        components: {
-          default: () => import(/* webpackChunkName: "create-story" */ './views/CreateUpdateStory.vue'),
-          footer: () => import(/* webpackChunkName: "footer" */ './views/Footer.vue'),
-        },
-      },
-
-      // view user profile
-      {
-        path: '/user/:username',
-        name: 'user',
-        meta: {
-          isModalView: true
-        },
-        components: {
-          default: () => import(/* webpackChunkName: "user" */ './views/UserProfile.vue'),
-          footer: () => import(/* webpackChunkName: "footer" */ './views/Footer.vue'),
-        },
-        props: true
-      },
-      // edit user profile
-      {
-        path: '/user/:user_key/edit',
-        name: 'edit-user',
-        components: {
-          default: () => import(/* webpackChunkName: "edit-user" */ './views/UserProfileEdit.vue'),
-          footer: () => import(/* webpackChunkName: "footer" */ './views/Footer.vue'),
-        }
-      },
-      // view / edit user settings
-      {
-        path: '/user/:id/settings',
-        name: 'user-settings',
-        components: {
-          default: () => import(/* webpackChunkName: "user-settings" */ './views/Settings.vue'),
-          footer: () => import(/* webpackChunkName: "footer" */ './views/Footer.vue'),
-        }
-      },
-      // contact us
-      {
-        path: '/contact',
-        name: 'contact',
-        // route level code-splitting
-        // this generates a separate chunk (contact.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        components: {
-          default: () => import(/* webpackChunkName: "contact" */ './views/Contact.vue'),
-          footer: () => import(/* webpackChunkName: "footer" */ './views/Footer.vue'),
-        }
-      },
-      // about us
-      {
-        path: '/about',
-        name: 'about',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        components: {
-          default: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
-          footer: () => import(/* webpackChunkName: "footer" */ './views/Footer.vue'),
-        }
-      },
-      // recover password
-      {
-        path: '/auth/reset-password/:reset_id',
-        name: 'resetPassword',
-        // route level code-splitting
-        // this generates a separate chunk (recover-password.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        components: {
-          default: () => import(/* webpackChunkName: "recover-password" */ './views/RecoverPassword.vue'),
-          footer: () => import(/* webpackChunkName: "footer" */ './views/Footer.vue'),
-        }
-      },
-      // validate email feedback
-      {
-        path: '/users/validate-email/:validate_id',
-        name: 'validateEmail',
-        // route level code-splitting
-        // this generates a separate chunk (validate-email.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        components: {
-          default: () => import(/* webpackChunkName: "validate-email" */ './views/ValidateEmail.vue'),
-          footer: () => import(/* webpackChunkName: "footer" */ './views/Footer.vue'),
-        }
-      },
-      // validate email feedback
-      {
-        path: '/test',
-        name: 'test',
-        components: {
-          default: () => import(/* webpackChunkName: "validate-email" */ './views/Test.vue'),
-        }
-      },
-      {
-        path: '*',
-        name: 'error',
-        component: Error
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    // home page
+    {
+      path: '/',
+      name: 'home',
+      component: Home,
+      meta: {
+        isModalView: true
       }
+    },
+    {
+      path: '/search',
+      name: 'search',
+      components: {
+        default: () => import( /* webpackChunkName: "search-stories" */ './views/Search.vue'),
+      },
+      meta: {
+        isModalView: true
+      }
+    },
+    // list my stories
+    {
+      path: '/user/:username/stories',
+      name: 'user-stories',
+      components: {
+        default: () => import( /* webpackChunkName: "user-stories" */ './views/UserStoriesList.vue'),
+        footer: () => import( /* webpackChunkName: "footer" */ './views/Footer.vue'),
+      },
+      meta: {
+        isModalView: true
+      }
+    },
+    {
+      path: '/user/:username/starred',
+      name: 'starred',
+      components: {
+        default: () => import( /* webpackChunkName: "starred-stories" */ './views/Starred.vue'),
+      },
+      meta: {
+        isModalView: true
+      }
+    },
+    // create a story
+    {
+      path: '/story/create',
+      name: 'create-story',
+      components: {
+        default: () => import( /* webpackChunkName: "create-story" */ './views/CreateUpdateStory.vue'),
+        footer: () => import( /* webpackChunkName: "footer" */ './views/Footer.vue'),
+      },
+    },
+    // view a story
+    {
+      path: '/story/:slug',
+      name: 'view-story',
+      components: {
+        // default: () => import(/* webpackChunkName: "story" */ './views/StoryStandalone.vue'),
+        footer: () => import( /* webpackChunkName: "footer" */ './views/Footer.vue'),
+      },
+      beforeEnter: (to, from, next) => {
+        const isModalView = from.matched.some(view => view.meta && view.meta.isModalView);
+        // console.log(from.matched);
+        if (!isModalView) {
+          // For direct access
+          to.matched[0].components = {
+            default: StoryStandalone,
+            modal: false
+          }
+        }
+        if (isModalView) {
+          // For isModalView access
+          if (to.matched[0].components) {
+            // Rewrite components for `default`
+            to.matched[0].components.default = from.matched[0].components.default
+            // Rewrite components for `modal`
+            to.matched[0].components.storyModal = StoryModal;
+          }
+        }
+        next();
+      },
+    },
+    {
+      path: '/story/:key/edit',
+      name: 'edit-story',
+      components: {
+        default: () => import( /* webpackChunkName: "create-story" */ './views/CreateUpdateStory.vue'),
+        footer: () => import( /* webpackChunkName: "footer" */ './views/Footer.vue'),
+      },
+    },
 
-    ],
-    // eslint-disable-next-line
-    scrollBehavior(to, from, savedPosition) {
-      return { x: 0, y: 0 }
+    // view user profile
+    {
+      path: '/user/:username',
+      name: 'user',
+      meta: {
+        isModalView: true
+      },
+      components: {
+        default: () => import( /* webpackChunkName: "user" */ './views/UserProfile.vue'),
+        footer: () => import( /* webpackChunkName: "footer" */ './views/Footer.vue'),
+      },
+      props: true
+    },
+    // edit user profile
+    {
+      path: '/user/:user_key/edit',
+      name: 'edit-user',
+      components: {
+        default: () => import( /* webpackChunkName: "edit-user" */ './views/UserProfileEdit.vue'),
+        footer: () => import( /* webpackChunkName: "footer" */ './views/Footer.vue'),
+      }
+    },
+    // view / edit user settings
+    {
+      path: '/user/:id/settings',
+      name: 'user-settings',
+      components: {
+        default: () => import( /* webpackChunkName: "user-settings" */ './views/Settings.vue'),
+        footer: () => import( /* webpackChunkName: "footer" */ './views/Footer.vue'),
+      }
+    },
+    // contact us
+    {
+      path: '/contact',
+      name: 'contact',
+      // route level code-splitting
+      // this generates a separate chunk (contact.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      components: {
+        default: () => import( /* webpackChunkName: "contact" */ './views/Contact.vue'),
+        footer: () => import( /* webpackChunkName: "footer" */ './views/Footer.vue'),
+      }
+    },
+    // about us
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      components: {
+        default: () => import( /* webpackChunkName: "about" */ './views/About.vue'),
+        footer: () => import( /* webpackChunkName: "footer" */ './views/Footer.vue'),
+      }
+    },
+    // recover password
+    {
+      path: '/auth/reset-password/:reset_id',
+      name: 'resetPassword',
+      // route level code-splitting
+      // this generates a separate chunk (recover-password.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      components: {
+        default: () => import( /* webpackChunkName: "recover-password" */ './views/RecoverPassword.vue'),
+        footer: () => import( /* webpackChunkName: "footer" */ './views/Footer.vue'),
+      }
+    },
+    // validate email feedback
+    {
+      path: '/users/validate-email/:validate_id',
+      name: 'validateEmail',
+      // route level code-splitting
+      // this generates a separate chunk (validate-email.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      components: {
+        default: () => import( /* webpackChunkName: "validate-email" */ './views/ValidateEmail.vue'),
+        footer: () => import( /* webpackChunkName: "footer" */ './views/Footer.vue'),
+      }
+    },
+    // validate email feedback
+    {
+      path: '/test',
+      name: 'test',
+      components: {
+        default: () => import( /* webpackChunkName: "validate-email" */ './views/Test.vue'),
+      }
+    },
+    {
+      path: '*',
+      name: 'error',
+      component: Error
     }
-  })
+
+  ],
+  // eslint-disable-next-line
+  scrollBehavior(to, from, savedPosition) {
+    return {
+      x: 0,
+      y: 0
+    }
+  }
+})
